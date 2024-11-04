392157f3c89af910948cd8d5940e2e049e250dc9
Jordan Castro <jordan@grupojvm.com>
2024-11-03 20:39:45 -0600

    remove js files

--- dom.js
+++
@@ -1,4 +0,0 @@
-function get_element_by_id(id) { document.getElementById(id); }
-function get_elements_by_class_name(class_name) { document.getElementsByClassName(class_name); }
-function get_elements_by_tag(tag) { return document.getElementsByTagName(tag); }
-function get_element_by_tag(tag) { return get_elements_by_tag(tag)[0]; }
