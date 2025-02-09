5bdc5fb91f336c4da100d1d9e6c40e2f181e3ac1
Jordan Castro <jordan@grupojvm.com>
2025-02-09 00:28:12 -0600

    First wasm compilation.

--- src/emitter/mod.rs
+++ src/emitter/mod.rs
@@ -1,1 +1,4 @@
 pub mod wasm_emitter;
+mod signatures;
+mod fn_builder;
+mod utils;
---
+++ src/emitter/utils.rs
@@ -0,0 +1,17 @@
+use wasm_encoder::ValType;
+
+use crate::parser::ast::Expression;
+
+pub fn get_param_type(param: Expression) -> ValType {
+    match param {
+        Expression::Identifier(tk, name) => match name.as_ref() {
+            "i32" => ValType::I32,
+            "f32" => ValType::F32,
+            "f64" => ValType::F64,
+            "i64" => ValType::I64,
+            "bool" => ValType::I32,
+            _ => panic!("Unsupported type"),
+        },
+        _ => panic!("Unsupported type"),
+    }
+}
